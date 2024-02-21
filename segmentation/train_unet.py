7fea6a4b9a5f31f578fd577623970661b7823652
Afterstone <invisiblex@gmail.com>
2024-02-21 14:20:57 +0000

    Save old unet changes

--- segmentation/train_unet.py
+++ segmentation/train_unet.py
@@ -1,5 +1,8 @@
+import datetime as dt
 import typing as t
+from calendar import c
 from pathlib import Path
+from typing import Any
 
 import lightning as L
 import matplotlib.pyplot as plt
@@ -9,9 +12,12 @@ import torchvision.transforms as TVT
 import torchvision.transforms.functional as TVTF
 from lightning.pytorch.callbacks import Checkpoint
 from lightning.pytorch.callbacks.callback import Callback
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from lightning.pytorch.loggers import TensorBoardLogger
 from lightning.pytorch.loggers.logger import Logger
+from matplotlib import axis
 from matplotlib.pylab import f
+from pandas import isna
 from torch.utils.data import DataLoader
 
 from segmentation.datasets.seg_food import SegFoodDataset
@@ -101,8 +107,13 @@ class Unet(L.LightningModule):
         test_thresh: float = 0.5,
         mask_norm_constant: float = 0.5,
         augmentations: AugmentationList | None = None,
+        log_dir: Path = Path('.'),
     ):
         super().__init__()
+
+        self.log_dir = log_dir / 'images'
+        if not self.log_dir.exists():
+            self.log_dir.mkdir(parents=True)
 
         if test_data_X.shape[0] > 10:
             raise ValueError('test_data must be a tensor of shape (N, 3, H, W) where N < 10')
@@ -125,10 +136,10 @@ class Unet(L.LightningModule):
         self.enc3 = T.nn.Conv2d(64, 128, 3, padding=1)
         self.enc4 = T.nn.Conv2d(128, 256, 3, padding=1)
 
-        # self.ebn1 = T.nn.BatchNorm2d(32)
-        # self.ebn2 = T.nn.BatchNorm2d(64)
-        # self.ebn3 = T.nn.BatchNorm2d(128)
-        # self.ebn4 = T.nn.BatchNorm2d(256)
+        self.ebn1 = T.nn.BatchNorm2d(32)
+        self.ebn2 = T.nn.BatchNorm2d(64)
+        self.ebn3 = T.nn.BatchNorm2d(128)
+        self.ebn4 = T.nn.BatchNorm2d(256)
 
         self.dec4 = T.nn.Conv2d(256, 128, 3, padding=1)
         self.dec3 = T.nn.Conv2d(256, 64, 3, padding=1)
@@ -145,16 +156,17 @@ class Unet(L.LightningModule):
 
     def forward(self, X: T.Tensor) -> T.Tensor:
         X = self.bn1(X)
-        E1 = self.relu(self.enc1(X))
-        E2 = self.relu(self.enc2(self.maxpool(E1)))
-        E3 = self.relu(self.enc3(self.maxpool(E2)))
-        E4 = self.relu(self.enc4(self.maxpool(E3)))
+
+        E1 = self.relu(self.ebn1(self.enc1(X)))
+        E2 = self.relu(self.ebn2(self.enc2(self.maxpool(E1))))
+        E3 = self.relu(self.ebn3(self.enc3(self.maxpool(E2))))
+        E4 = self.relu(self.ebn4(self.enc4(self.maxpool(E3))))
 
         D4 = self.relu(self.dbn4(self.dec4(E4)))
         D4 = T.nn.functional.interpolate(D4, scale_factor=2, mode='bilinear', align_corners=True)
-        D3 = self.relu(self.dbn3(self.dec3(T.concat([E3, D4], dim=1))))
+        D3 = self.relu(self.dbn3(self.dec3(T.concat([E3.real, D4], dim=1))))
         D3 = T.nn.functional.interpolate(D3, scale_factor=2, mode='bilinear', align_corners=True)
-        D2 = self.relu(self.dbn2(self.dec2(T.concat([E2, D3], dim=1))))
+        D2 = self.relu(self.dbn2(self.dec2(T.concat([E2.real, D3], dim=1))))
         D2 = T.nn.functional.interpolate(D2, scale_factor=2, mode='bilinear', align_corners=True)
         D1 = self.dbn1(self.dec1(T.concat([E1, D2], dim=1)))
         return D1
@@ -183,6 +195,43 @@ class Unet(L.LightningModule):
         y_hat = self.forward(X)
         loss = T.nn.functional.binary_cross_entropy_with_logits(y_hat, y, pos_weight=T.tensor(self.mask_norm_constant))
         self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+
+        with T.no_grad():
+            y_class = self.sigmoid(y_hat)
+            y_class[y_class > self.test_thresh] = 1.0
+            y_class[y_class <= self.test_thresh] = 0.0
+            y_class = y_class.long()
+            y_long = y.long()
+
+            acc = T.sum(y_class == y_long) / T.numel(y_long)
+            self.log('train_acc', acc, on_epoch=True,  on_step=False, logger=True)
+
+            # Shortcut: Since we are using binary labels, we can simply sum over the entire tensor
+            # to find the number of true positives, false positives, and false negatives.
+            # We can use y_hat * y to find the true positives, since we've thresholded y_hat.
+            TP = T.sum(y_class * y).item()
+            FP = T.sum(y_class * (1 - y)).item()
+            FN = T.sum((1 - y_class) * y).item()
+
+            # TP / (TP + FP), i.e. where y_hat == y divided by predicted positives.
+            # Check if sum of y_class == 0 or if it contains nans.
+            precision = 0.0 if int(TP) == 0 else TP / (TP + FP)
+            self.log('train_precision', precision, on_epoch=True,  on_step=False, logger=True)
+
+            # TP / (TP + FN), i.e. where y_hat == y divided by actual positives.
+            recall = 0.0 if int(TP) == 0 else TP / (TP + FN)
+            self.log('train_recall', recall, on_epoch=True,  on_step=False, logger=True)
+
+            # 2 * (precision * recall) / (precision + recall)
+            if int(FP + FN) == 0:
+                f1_score = 0.0
+            else:
+                f1_score = 2 * (precision * recall) / (precision + recall)
+            self.log('train_f1_score', f1_score, on_epoch=True,  on_step=False, logger=True)
+
+            # IoU = TP / (TP + FP + FN)
+            iou = (TP / (T.sum(y_class + y_long) - TP)).item()
+            self.log('train_iou', iou, on_epoch=True,  on_step=False, logger=True)
 
         return loss
 
@@ -222,21 +271,66 @@ class Unet(L.LightningModule):
 
             # Get current epoch
             epoch = self.current_epoch
-            fig.savefig(f'./test_{epoch}.png')
+            fig.savefig(self.log_dir / f'./test_{epoch}.png')
+
+    def validation_step(self, batch: t.Tuple[T.Tensor, T.Tensor], _: int) -> None:
+        X, y = batch
+        y_hat = self.forward(X.detach())
+
+        loss = T.nn.functional.binary_cross_entropy_with_logits(y_hat, y, pos_weight=T.tensor(self.mask_norm_constant))
+        self.log('val_loss', loss, on_step=False, on_epoch=True, logger=True)
+
+        y_class = self.sigmoid(y_hat)
+        y_class[y_class > self.test_thresh] = 1.0
+        y_class[y_class <= self.test_thresh] = 0.0
+        y_class = y_class.long()
+        y_long = y.long()
+
+        acc = T.sum(y_class == y_long) / T.numel(y_long)
+        self.log('val_acc', acc, on_epoch=True,  on_step=False, logger=True)
+
+        # Shortcut: Since we are using binary labels, we can simply sum over the entire tensor
+        # to find the number of true positives, false positives, and false negatives.
+        # We can use y_hat * y to find the true positives, since we've thresholded y_hat.
+        TP = T.sum(y_class * y).item()
+        FP = T.sum(y_class * (1 - y)).item()
+        FN = T.sum((1 - y_class) * y).item()
+
+        # TP / (TP + FP), i.e. where y_hat == y divided by predicted positives.
+        # Check if sum of y_class == 0 or if it contains nans.
+        precision = 0.0 if int(TP) == 0 else TP / (TP + FP)
+        self.log('val_precision', precision, on_epoch=True,  on_step=False, logger=True)
+
+        # TP / (TP + FN), i.e. where y_hat == y divided by actual positives.
+        recall = 0.0 if int(TP) == 0 else TP / (TP + FN)
+        self.log('val_recall', recall, on_epoch=True,  on_step=False, logger=True)
+
+        # 2 * (precision * recall) / (precision + recall)
+        if int(TP) == 0 or int(precision + recall) < 1e-10:
+            f1_score = 0.0
+        else:
+            f1_score = 2 * (precision * recall) / (precision + recall)
+        self.log('val_f1_score', f1_score, on_epoch=True,  on_step=False, logger=True)
+
+        # IoU = TP / (TP + FP + FN)
+        iou = (TP / (T.sum(y_class + y_long) - TP)).item()
+        self.log('val_iou', iou, on_epoch=True,  on_step=False, logger=True)
 
 
 def main(
     log_dir: Path = Path('./logs/unet/'),
     debug: bool = False,
 ):
+    log_name = 'unet_debug' if debug else 'unet'
+    log_name = f"{log_name}_{dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
     if debug:
         ds_train = SegFoodDataset(root=Path('./data/seg_food/test'), load_first_n=100)
-        dl_train = DataLoader(dataset=ds_train, batch_size=32, shuffle=True)
         ds_test = ds_train
     else:
         ds_train = SegFoodDataset(root=Path('./data/seg_food/train'))
-        dl_train = DataLoader(dataset=ds_train, batch_size=32, shuffle=True)
         ds_test = SegFoodDataset(root=Path('./data/seg_food/test'))
+    dl_train = DataLoader(dataset=ds_train, batch_size=32, shuffle=True)
+    dl_test = DataLoader(dataset=ds_test, batch_size=4, shuffle=False)
 
     augmentations = AugmentationList([
         RandomVerticalFlip(p=0.5),
@@ -265,13 +359,17 @@ def main(
         test_data_y=ds_test.y[:10],
         mask_norm_constant=1 - float(ds_train.y.mean().item()),
         augmentations=augmentations,
+        log_dir=log_dir / log_name,
+        lr=1e-3,
     )
 
-    loggers: list[Logger] = [TensorBoardLogger(log_dir)]
-    callbacks: list[Callback | Checkpoint] = []
+    loggers: list[Logger] = [TensorBoardLogger(log_dir, name=log_name)]
+    callbacks: list[Callback | Checkpoint] = [
+        EarlyStopping(monitor='val_loss', patience=5, mode='min'),
+    ]
 
     trainer = L.Trainer(max_epochs=50, accelerator='auto', logger=loggers, callbacks=callbacks)
-    trainer.fit(model=model, train_dataloaders=dl_train)
+    trainer.fit(model=model, train_dataloaders=dl_train, val_dataloaders=dl_test)
 
 
 if __name__ == '__main__':
