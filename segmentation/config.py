27de0e9a7074c08898d5afd96260c6931d64ab23
Afterstone <invisiblex@gmail.com>
2024-03-08 13:04:01 +0000

    Add ViT/B to SAM

--- segmentation/config.py
+++ segmentation/config.py
@@ -2,11 +2,11 @@ from pathlib import Path
 
 import torch as T
 
-MODEL_NAME: str = 'convnext_xxlarge'
-MODEL_WEIGHTS_NAME: str = 'laion2b_s34b_b82k_augreg'
+# MODEL_NAME: str = 'convnext_xxlarge'
+# MODEL_WEIGHTS_NAME: str = 'laion2b_s34b_b82k_augreg'
 
-# MODEL_NAME: str = 'convnext_base'
-# MODEL_WEIGHTS_NAME: str = 'laion400m_s13b_b51k'
+MODEL_NAME: str = 'convnext_base'
+MODEL_WEIGHTS_NAME: str = 'laion400m_s13b_b51k'
 
 # MODEL_NAME: str = 'RN50x64'
 # MODEL_WEIGHTS_NAME: str = 'openai'
@@ -16,8 +16,11 @@ MODEL_WEIGHTS_NAME: str = 'laion2b_s34b_b82k_augreg'
 
 TORCH_DEVICE: str | T.device = 'cuda' if T.cuda.is_available() else 'cpu'
 
-SAM_CHECKPOINT: str = "models/sam_vit_h_4b8939.pth"
-MODEL_TYPE: str = "vit_h"
+# SAM_CHECKPOINT: str = "models/sam_vit_h_4b8939.pth"
+# MODEL_TYPE: str = "vit_h"
+
+SAM_CHECKPOINT: str = "models/sam_vit_b_01ec64.pth"
+MODEL_TYPE: str = "vit_b"
 
 
 FOODSEG103_ROOT: Path = Path('./data/FoodSeg103')
