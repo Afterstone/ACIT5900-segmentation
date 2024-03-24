4b164ee0d77bb2e244469413f24b2938a2ce878e
Afterstone <invisiblex@gmail.com>
2024-03-24 16:09:33 +0000

    Lots of stuff!
    config.py:
    - Rename envvar variables
    
    test_gradcam_openclip_sam.py:
    - Various changes

--- segmentation/config.py
+++ segmentation/config.py
@@ -2,17 +2,17 @@ from pathlib import Path
 
 import torch as T
 
-# MODEL_NAME: str = 'convnext_xxlarge'
-# MODEL_WEIGHTS_NAME: str = 'laion2b_s34b_b82k_augreg'
+# CLIP_MODEL_NAME: str = 'convnext_xxlarge'
+# CLIP_MODEL_WEIGHTS_NAME: str = 'laion2b_s34b_b82k_augreg'
 
 CLIP_MODEL_NAME: str = 'convnext_base'
 CLIP_MODEL_WEIGHTS_NAME: str = 'laion400m_s13b_b51k'
 
-# MODEL_NAME: str = 'RN50x64'
-# MODEL_WEIGHTS_NAME: str = 'openai'
+# CLIP_MODEL_NAME: str = 'RN50x64'
+# CLIP_MODEL_WEIGHTS_NAME: str = 'openai'
 
-# MODEL_NAME: str = 'RN101-quickgelu'
-# MODEL_WEIGHTS_NAME: str = 'openai'
+# CLIP_MODEL_NAME: str = 'RN101-quickgelu'
+# CLIP_MODEL_WEIGHTS_NAME: str = 'openai'
 
 # CLIP_CLASSIFIER_NAME: str = 'ViT-H-14'
 # CLIP_CLASSIFIER_WEIGHTS_NAME: str = 'laion2b_s32b_b79k'
--- segmentation/test_gradcam_openclip_sam.py
+++ segmentation/test_gradcam_openclip_sam.py
@@ -70,6 +70,7 @@ class ClipClassifierResult:
 
 
 class ClipClassifier:
+    # TODO: Make an interface for the classifier.
     def __init__(
         self,
         model_name: str,
@@ -108,6 +109,7 @@ class ClipClassifier:
 
 
 class ClipAttentionMapper:
+    # TODO: Make an interface for the attention mapper.
     def __init__(
         self,
         model_name: str,
@@ -170,13 +172,40 @@ def propose_points(attn_map: np.ndarray, n_points: int = 3, norm: int = 2) -> np
     return input_points
 
 
+def extract_class_indices_from_annotations(annotations: T.Tensor) -> T.Tensor:
+    return annotations.ravel().unique().squeeze()
+
+
+def get_sparse_annotation_masks(annotations: T.Tensor) -> dict[int, T.Tensor]:
+    masks: dict[int, T.Tensor] = {}
+    indices = [int(i) for i in extract_class_indices_from_annotations(annotations)]
+    for i in indices:
+        masks[i] = (annotations == i).float()
+    return masks
+
+
+@dataclass
+class ClipAttentionMapperConfig:
+    model_name: str
+    model_weights_name: str
+
+
+@dataclass
+class ClipClassifierConfig:
+    model_name: str
+    model_weights_name: str
+
+
+@dataclass
+class SamConfig:
+    model_type: str
+    checkpoint: str
+
+
 def main(
-    clip_model_name: str,
-    clip_model_weights_name: str,
-    clip_model_cls_name: str,
-    clip_model_cls_weights_name: str,
-    sam_checkpoint: str,
-    sam_model_type: str,
+    clip_attn_mapper_config: ClipAttentionMapperConfig,
+    clip_cls_config: ClipClassifierConfig,
+    sam_config: SamConfig,
     foodseg103_root: Path,
     prefix: str = "The dish contains the following: ",
     device: str | T.device = 'cuda',
@@ -186,20 +215,24 @@ def main(
     texts = ds_test.category_df['category'].tolist()
 
     with T.no_grad(), T.cuda.amp.autocast():  # type: ignore
-        print(f"Loading CLIP model {clip_model_name}...")
-        attn_mapper = ClipAttentionMapper(clip_model_name, clip_model_weights_name, texts, device)
+        print(f"Loading CLIP model {clip_attn_mapper_config.model_name}...")
+        attn_mapper = ClipAttentionMapper(clip_attn_mapper_config.model_name,
+                                          clip_attn_mapper_config.model_weights_name, texts, device)
 
-        print(f"Loading CLIP classifier model {clip_model_cls_name}...")
-        clip_classifier = ClipClassifier(clip_model_cls_name, clip_model_cls_weights_name, texts, device)
+        # print(f"Loading CLIP classifier model {clip_cls_config.model_name}...")
+        # clip_classifier = ClipClassifier(clip_cls_config.model_name, clip_cls_config.model_weights_name, texts, device)
 
-        print(f"Loading SAM model {sam_model_type}...")
-        sam_predictor = get_sam_model(sam_checkpoint=sam_checkpoint, sam_model_type=sam_model_type, device=device)
+        print(f"Loading SAM model {sam_config.model_type}...")
+        sam_predictor = get_sam_model(sam_checkpoint=sam_config.checkpoint,
+                                      sam_model_type=sam_config.model_type, device=device)
 
+    ious = defaultdict(list)
     for idx in trange(0, len(ds_test)):
         # Load the annotations.
         with Image.open(ds_test.annotations_paths[idx]) as img:
             ann_tensor = T.tensor(np.array(img))
-            # TODO: Consider getting masks for the known classes instead/in addition to clip_classifier.
+            ann_indices = [int(i) for i in extract_class_indices_from_annotations(ann_tensor.unsqueeze(0))]
+            ann_mask_lookup = get_sparse_annotation_masks(ann_tensor.unsqueeze(0))
 
         image_path = ds_test.image_paths[idx]
         with Image.open(image_path) as img:
@@ -207,16 +240,20 @@ def main(
             image_np = np.array(img).astype(np.float32) / 255.
 
             with T.no_grad(), T.cuda.amp.autocast():  # type: ignore
-                cls_res = clip_classifier.classify(img, 10)
-                top_texts = cls_res.texts
-                top_text_features = attn_mapper.text_features[cls_res.indices]
+                # cls_res = clip_classifier.classify(img, top_k=10)
+                # indices = cls_res.indices
+
+                indices_list = ann_indices
+                top_text_features_list = [attn_mapper.text_features[int(i)] for i in indices_list]
+                top_text_features = T.stack(top_text_features_list)
+                top_texts = [texts[i] for i in indices_list]
 
             with T.cuda.amp.autocast():  # type: ignore
                 attn_maps = attn_mapper.get_attention_maps(img, top_texts, top_text_features)
 
         image_sam = (image_np.copy() * 255.0).astype(np.uint8)
         sam_predictor.set_image(image_sam)
-        for _, atmap in attn_maps.items():
+        for i, (_, atmap) in enumerate(attn_maps.items()):
             atmap = np.array(Image.fromarray(atmap).resize((image_np.shape[1], image_np.shape[0])))  # type: ignore
             input_points = propose_points(np.array(atmap), n_points=3)
             input_labels = np.array([1] * len(input_points))
@@ -227,54 +264,89 @@ def main(
                 multimask_output=True,
             )
             best_mask = masks[np.argmax(scores), :, :]
-            # TODO: Compare mask against test dataset.
+            best_mask_tensor = T.tensor(best_mask).ravel()
 
-        # n_cols, n_rows = 3, len(attn_maps.keys())
-        # fig, axes = plt.subplots(n_rows, n_cols, figsize=(n_cols*5, n_rows*5))
-        # fig.suptitle(f"Model: {clip_model_name}\nWeights: {clip_model_weights_name}")
-        # for i, (text_prob, (text, atmap)) in enumerate(zip(topk_text_probs, attn_maps.items())):
-        #     ax_orig, ax_attn, ax_sam = axes[i]
-        #     for ax in axes[i]:
-        #         ax.axis("off")
+            annotation = ann_mask_lookup[indices_list[i]].squeeze()
+            annotation = T.from_numpy(np.array(Image.fromarray(annotation.numpy()).resize(
+                (image_np.shape[1], image_np.shape[0]))))
+            annotation_bool = annotation > 0
+            annotation_bool = annotation_bool.ravel()
 
-        #     ax_orig.imshow(image_np)
-        #     ax_orig.set_title("Original")
+            intersection = T.logical_and(best_mask_tensor, annotation_bool)
+            union = T.logical_or(best_mask_tensor, annotation_bool)
+            iou = T.true_divide(intersection.sum(), union.sum())
 
-        #     atmap = np.array(Image.fromarray(atmap).resize((image_np.shape[1], image_np.shape[0])))
-        #     ax_attn.imshow(atmap, alpha=0.5, cmap="jet")
-        #     ax_attn.set_title(f"{text} - {text_prob:.4f}")
+            ious[indices_list[i]].append(iou.item())
 
-        #     input_points = propose_points(np.array(atmap), n_points=3)
-        #     input_labels = np.array([1] * len(input_points))
-        #     show_points(input_points, input_labels, ax_attn)
+            if idx > 0 and idx % 100 == 0:
+                means = []
+                for i, iou_list in sorted(list(ious.items()), key=lambda x: x[0]):
+                    means.append(np.mean(iou_list))
+                    print(f"Class {i: 3d}: {means[-1]:.4f}")
+                print("-"*10)
+                print(f"mIOU: {np.mean(means):.4f}")
+                print()
 
-        #     image_sam = (image_np.copy() * 255.0).astype(np.uint8)
-        #     sam_predictor.set_image(image_sam)
-        #     masks, scores, logits = sam_predictor.predict(
-        #         point_coords=input_points,
-        #         point_labels=input_labels,
-        #         multimask_output=True,
-        #     )
+    means = []
+    for i, iou_list in sorted(list(ious.items()), key=lambda x: x[0]):
+        means.append(np.mean(iou_list))
+        print(f"Class {i: 3d}: {means[-1]:.4f}")
+    print("-"*10)
+    print(f"mIOU: {np.mean(means):.4f}")
+    print()
 
-        #     ax_sam.imshow(image_np)
-        #     best_mask = masks[np.argmax(scores), :, :]
-        #     show_mask(best_mask[np.newaxis, :, :], ax_sam, random_color=True)
-        #     show_points(input_points, input_labels, ax_sam)
+    # n_cols, n_rows = 3, len(attn_maps.keys())
+    # fig, axes = plt.subplots(n_rows, n_cols, figsize=(n_cols*5, n_rows*5))
+    # fig.suptitle(f"Model: {clip_model_name}\nWeights: {clip_model_weights_name}")
+    # for i, (text_prob, (text, atmap)) in enumerate(zip(topk_text_probs, attn_maps.items())):
+    #     ax_orig, ax_attn, ax_sam = axes[i]
+    #     for ax in axes[i]:
+    #         ax.axis("off")
 
-        #     fig.savefig("gradcam_openclip_sam.png")
-        #     print()
+    #     ax_orig.imshow(image_np)
+    #     ax_orig.set_title("Original")
 
-        # return
+    #     atmap = np.array(Image.fromarray(atmap).resize((image_np.shape[1], image_np.shape[0])))
+    #     ax_attn.imshow(atmap, alpha=0.5, cmap="jet")
+    #     ax_attn.set_title(f"{text} - {text_prob:.4f}")
+
+    #     input_points = propose_points(np.array(atmap), n_points=3)
+    #     input_labels = np.array([1] * len(input_points))
+    #     show_points(input_points, input_labels, ax_attn)
+
+    #     image_sam = (image_np.copy() * 255.0).astype(np.uint8)
+    #     sam_predictor.set_image(image_sam)
+    #     masks, scores, logits = sam_predictor.predict(
+    #         point_coords=input_points,
+    #         point_labels=input_labels,
+    #         multimask_output=True,
+    #     )
+
+    #     ax_sam.imshow(image_np)
+    #     best_mask = masks[np.argmax(scores), :, :]
+    #     show_mask(best_mask[np.newaxis, :, :], ax_sam, random_color=True)
+    #     show_points(input_points, input_labels, ax_sam)
+
+    #     fig.savefig("gradcam_openclip_sam.png")
+    #     print()
+
+    # return
 
 
 if __name__ == '__main__':
     main(
-        clip_model_name=config.CLIP_MODEL_NAME,
-        clip_model_weights_name=config.CLIP_MODEL_WEIGHTS_NAME,
-        clip_model_cls_name=config.CLIP_CLASSIFIER_NAME,
-        clip_model_cls_weights_name=config.CLIP_CLASSIFIER_WEIGHTS_NAME,
-        sam_checkpoint=config.SAM_CHECKPOINT,
-        sam_model_type=config.MODEL_TYPE,
+        clip_attn_mapper_config=ClipAttentionMapperConfig(
+            model_name=config.CLIP_MODEL_NAME,
+            model_weights_name=config.CLIP_MODEL_WEIGHTS_NAME,
+        ),
+        clip_cls_config=ClipClassifierConfig(
+            model_name=config.CLIP_CLASSIFIER_NAME,
+            model_weights_name=config.CLIP_CLASSIFIER_WEIGHTS_NAME,
+        ),
+        sam_config=SamConfig(
+            checkpoint=config.SAM_CHECKPOINT,
+            model_type=config.MODEL_TYPE,
+        ),
         device=config.TORCH_DEVICE,
         foodseg103_root=config.FOODSEG103_ROOT,
     )
